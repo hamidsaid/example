7b1346d1cdaf950a9c147b1f8201869fee2dbe01
hamidsaid <hamidsaid777@gmail.com>
2021-09-15 12:44:39 +0300

    first commit

---
+++ includes/navbar.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  
+  <title>UNIHUB</title>
+  <!-- 
+        <link rel="stylesheet" href="styles/bootstrap.min.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link rel="stylesheet" href="styles.css">
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap" rel="stylesheet"> -->
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="styles/main.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+  <!--Styles CSS-->
+  <link rel="stylesheet" href="styles.css">
+  
+  </head>
+
+<body style="font-size: 16px;;">
+  <nav class="navbar navbar-expand-lg navbar-light" style="background-color:white;">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.html"> <img src="picha/ICON.PNG" alt="" width="" height="">
+        UNIHUB</a></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link ms-lg-4" id="item" href="index.html">HOME</a>
+          </li>
+
+
+          <li class="nav-item ms-lg-4">
+            <a class="nav-link " id="item" href="login.php">SIGN IN</a>
+          </li>
+          <li class="nav-item ms-lg-4">
+            <a class="nav-link " id="item" href="register.php">REGISTER</a>
+          </li>
+          <li class="nav-item ms-lg-4">
+            <a class="nav-link " id="item" href="#">ABOUT</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
